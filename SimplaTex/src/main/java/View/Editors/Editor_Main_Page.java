265688fe47833002f9f4b38d74dd2e857e821488
OliverMineau <oliver.mineau42@orange.fr>
2023-07-24 01:13:36 +0200

    SimpleEditorHelper finished

--- SimplaTex/src/main/java/View/Editors/Editor_Main_Page.java
+++ SimplaTex/src/main/java/View/Editors/Editor_Main_Page.java
@@ -1,69 +1,35 @@
 package View.Editors;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-public class Editor_Main_Page extends JPanel {
+public class Editor_Main_Page extends SimpleEditorHelper {
 
-    Font labelFont;
-    Font textFieldFont;
-    GridBagConstraints gridBagConstraints;
+
 
     public Editor_Main_Page() {
 
-        GridBagLayout gridBagLayout = new GridBagLayout();
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.anchor = GridBagConstraints.CENTER; // Align elements to the top-left corner
-        gridBagConstraints.fill = GridBagConstraints.BOTH; // Elements will fill horizontally
-        gridBagConstraints.gridy = 0; // Start at the top (first row)
-        gridBagConstraints.weighty = 1;
-        gridBagConstraints.weightx = 1;
-
-        setLayout(gridBagLayout);
-
-        setBackground(Color.BLUE);
-
-        labelFont = new Font("Arial", Font.BOLD, 30);
-        textFieldFont = new Font("Arial", Font.PLAIN, 30);
-
-        createTextPanel("Logo Path :", new JTextField());
-        createTextPanel("Level of Education :", new JTextField());
-        createTextPanel("Field of Study :", new JTextField());
-        createTextPanel("Title :", new JTextField());
-        createTextPanel("Author Name:", new JTextField());
-        createTextPanel("Date :", new JTextField());
-        createTextAreaPanel("Text Area", new JTextArea());
+        JTextField imagePath = createImagePanel("Logo Image Path");
+        JTextField edLevel = createTextPanel("Level of Education :");
+        JTextField fieldStdy = createTextPanel("Field of Study :");
+        JTextField title = createTextPanel("Title :");
+        ArrayList<JTextField> authors = createMultipleTexts("Authors :");
+        JTextField date = createTextPanel("Date :");
     }
 
-    private void createTextPanel(String titleText, JTextField textField) {
-        JPanel jPanel = createTitle(titleText);
 
-        textField.setFont(textFieldFont);
-        jPanel.add(textField, BorderLayout.SOUTH);
 
-        gridBagConstraints.gridy++; // Increment the row number for the next element
-        add(jPanel, gridBagConstraints);
-    }
-
-    private void createTextAreaPanel(String titleText, JTextArea textArea) {
-        JPanel jPanel = createTitle(titleText);
-
-        textArea.setFont(textFieldFont);
-        textArea.setRows(5);
-        jPanel.add(textArea, BorderLayout.SOUTH);
-
-        gridBagConstraints.gridy++; // Increment the row number for the next element
-        add(jPanel, gridBagConstraints);
-    }
-
-    public JPanel createTitle(String titleText) {
-        BorderLayout borderLayout = new BorderLayout();
-        JPanel jPanel = new JPanel(borderLayout);
-        jPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-
-        JLabel label = new JLabel(titleText);
-        label.setFont(labelFont);
-        jPanel.add(label, BorderLayout.NORTH);
-        return jPanel;
-    }
 }
---
+++ SimplaTex/src/main/java/View/Editors/SimpleEditorHelper.java
@@ -0,0 +1,197 @@
+package View.Editors;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+
+public abstract class SimpleEditorHelper extends JPanel{
+
+    public Font labelFont;
+    public Font textFieldFont;
+    public GridBagConstraints gridBagConstraints;
+
+    public SimpleEditorHelper() {
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.anchor = GridBagConstraints.CENTER; // Align elements to the top-left corner
+        gridBagConstraints.fill = GridBagConstraints.BOTH; // Elements will fill horizontally
+        gridBagConstraints.gridy = 0; // Start at the top (first row)
+        gridBagConstraints.weighty = 1;
+        gridBagConstraints.weightx = 1;
+
+        setLayout(gridBagLayout);
+
+        labelFont = new Font("Arial", Font.BOLD, 30);
+        textFieldFont = new Font("Arial", Font.PLAIN, 30);
+    }
+
+    public JTextField createImagePanel(String titleText) {
+        JPanel jPanel = createTitle(titleText);
+
+        JTextField textField = new JTextField();
+        textField.setFont(textFieldFont);
+        jPanel.add(textField, BorderLayout.CENTER);
+
+        JLabel picLabel = new JLabel();
+        picLabel.setBorder(new EmptyBorder(15,0,0,0));
+        picLabel.setFont(textFieldFont);
+        picLabel.setText("No Image Found");
+        jPanel.add(picLabel, BorderLayout.SOUTH);
+
+        textField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent documentEvent) {
+                updateImage(documentEvent);
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent documentEvent) {
+                updateImage(documentEvent);
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent documentEvent) {
+                updateImage(documentEvent);
+            }
+
+            private void updateImage(DocumentEvent documentEvent){
+                try {
+                    BufferedImage myPicture = ImageIO.read(new File(documentEvent.getDocument().getText(0,documentEvent.getDocument().getLength())));
+                    picLabel.setIcon(new ImageIcon(myPicture));
+                    picLabel.setText("");
+                } catch (Exception e) {
+                    picLabel.setIcon(null);
+                    picLabel.setText("No Image Found");
+                }
+            }
+        });
+
+        gridBagConstraints.gridy++; // Increment the row number for the next element
+        add(jPanel, gridBagConstraints);
+
+        return textField;
+    }
+
+    public JTextField createTextPanel(String titleText) {
+        JPanel jPanel = createTitle(titleText);
+
+        JTextField textField = new JTextField();
+        textField.setFont(textFieldFont);
+        jPanel.add(textField, BorderLayout.SOUTH);
+
+        gridBagConstraints.gridy++; // Increment the row number for the next element
+        add(jPanel, gridBagConstraints);
+
+        return textField;
+    }
+
+    public JTextArea createTextAreaPanel(String titleText) {
+        JPanel jPanel = createTitle(titleText);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setFont(textFieldFont);
+        textArea.setRows(5);
+        jPanel.add(textArea, BorderLayout.SOUTH);
+
+        gridBagConstraints.gridy++; // Increment the row number for the next element
+        add(jPanel, gridBagConstraints);
+
+        return textArea;
+    }
+
+    public ArrayList<JTextField> createMultipleTexts(String titleText) {
+        JPanel jPanel = createTitle(titleText);
+        jPanel.setLayout(new GridLayout(0,1));
+
+        ArrayList<JTextField> jTextFields = new ArrayList<>();
+        jTextFields.add(new JTextField());
+        jTextFields.get(0).setFont(textFieldFont);
+        jPanel.add(jTextFields.get(0));
+
+        JPanel buttons = new JPanel(new GridLayout(0,2));
+        JButton addBtn = new JButton("Add");
+        JButton rmvBtn = new JButton("Remove");
+        rmvBtn.setVisible(false);
+
+        addBtn.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                jTextFields.add(new JTextField());
+                int index = jTextFields.size();
+                jPanel.add(jTextFields.get(index-1),index-1);
+
+                if(index > 1) rmvBtn.setVisible(true);
+
+                jPanel.revalidate();
+                jPanel.repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {}
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {}
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {}
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {}
+        });
+
+        rmvBtn.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                int index = jTextFields.size();
+
+                if(index == 1) return;
+
+                jTextFields.remove(index-1);
+                jPanel.remove(index-1);
+
+                if(jTextFields.size() == 1) {
+                    rmvBtn.setVisible(false);
+                    return;
+                }
+
+                jPanel.revalidate();
+                jPanel.repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {}
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {}
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {}
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {}
+        });
+        buttons.add(addBtn);
+        buttons.add(rmvBtn);
+
+        jPanel.add(buttons);
+
+
+        gridBagConstraints.gridy++; // Increment the row number for the next element
+        add(jPanel, gridBagConstraints);
+
+        return jTextFields;
+    }
+
+    public JPanel createTitle(String titleText) {
+        BorderLayout borderLayout = new BorderLayout();
+        JPanel jPanel = new JPanel(borderLayout);
+        jPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        JLabel label = new JLabel(titleText);
+        label.setFont(labelFont);
+        jPanel.add(label, BorderLayout.NORTH);
+        return jPanel;
+    }
+}
