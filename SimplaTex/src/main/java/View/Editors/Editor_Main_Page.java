ba754318a1b8c716a99d75cfcd6b7b2095868ccd
OliverMineau <oliver.mineau42@orange.fr>
2023-07-23 22:24:43 +0200

    SimpleEditor v1

--- SimplaTex/src/main/java/View/Editors/Editor_Main_Page.java
+++ SimplaTex/src/main/java/View/Editors/Editor_Main_Page.java
@@ -1,6 +1,60 @@
 package View.Editors;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Editor_Main_Page extends JPanel {
+
+    Font labelFont;
+    Font textFieldFont;
+
+    public Editor_Main_Page() {
+        GridLayout gridLayout = new GridLayout(0, 1);
+        gridLayout.setVgap(30);
+        setLayout(gridLayout);
+        setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
+
+
+        labelFont = new Font("Arial", Font.BOLD, 30);
+        textFieldFont = new Font("Arial", Font.PLAIN, 30);
+
+        // Add rows (each row contains a JLabel and a JTextField)
+        createTextPanel("Logo Path :", new JTextField());
+        createTextPanel("Level of Education :", new JTextField());
+        createTextPanel("Field of Study :", new JTextField());
+        createTextPanel("Title :", new JTextField());
+        createTextPanel("Author Name:", new JTextField());
+        createTextPanel("Date :", new JTextField());
+        createTextAreaPanel("Text Area", new JTextArea());
+    }
+
+    private void createTextPanel(String titleText, JTextField textField) {
+        JPanel jPanel = createTitle(titleText);
+
+        textField.setFont(textFieldFont);
+        jPanel.add(textField, BorderLayout.SOUTH);
+
+        add(jPanel);
+    }
+
+    private void createTextAreaPanel(String titleText, JTextArea textArea) {
+        JPanel jPanel = createTitle(titleText);
+
+        textArea.setFont(textFieldFont);
+        textArea.setRows(5);
+        jPanel.add(textArea, BorderLayout.SOUTH);
+
+        add(jPanel);
+    }
+
+    public JPanel createTitle(String titleText){
+        BorderLayout borderLayout = new BorderLayout();
+        JPanel jPanel = new JPanel(borderLayout);
+        jPanel.setBorder(BorderFactory.createEmptyBorder(0, 15, 0, 15));
+
+        JLabel label = new JLabel(titleText);
+        label.setFont(labelFont);
+        jPanel.add(label, BorderLayout.NORTH);
+        return jPanel;
+    }
 }
--- SimplaTex/src/main/java/View/SimpleEditor.java
+++ SimplaTex/src/main/java/View/SimpleEditor.java
@@ -3,8 +3,13 @@ package View;
 import Controller.Controller;
 import Model.Manager;
 import Patterns.Observer;
+import View.Editors.Editor_Main_Page;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.filechooser.FileView;
+import java.awt.*;
+import java.io.File;
 
 public class SimpleEditor extends JPanel implements Observer {
 
@@ -14,5 +19,47 @@ public class SimpleEditor extends JPanel implements Observer {
     public SimpleEditor(Manager manager, Controller controller) {
         this.manager = manager;
         this.controller = controller;
+
+        setLayout(new GridLayout(1,2));
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BorderLayout());
+        leftPanel.add(new JScrollPane(new Editor_Main_Page()), BorderLayout.CENTER);
+
+        JFileChooser rightPanel = new JFileChooser();
+        rightPanel.setDragEnabled(true);
+        rightPanel.setControlButtonsAreShown(false);
+        rightPanel.setCurrentDirectory(controller.getDownloadPath().toFile());
+        rightPanel.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        //TODO Marche pas affichage icons
+        rightPanel.setFileView(new FileView(){
+            public Icon getIcon(File f)
+            {
+                return FileSystemView.getFileSystemView().getSystemIcon(f);
+            }
+        });
+
+
+        //rightPanel.setFileFilter( new FolderFilter());
+
+        add(leftPanel);
+        add(rightPanel);
     }
+
+
+    private class FolderFilter extends javax.swing.filechooser.FileFilter {
+        @Override
+        public boolean accept( File file ) {
+            return file.isDirectory();
+        }
+
+        @Override
+        public String getDescription() {
+            return "We only take directories";
+        }
+    }
+
 }
+
+
